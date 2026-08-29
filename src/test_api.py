7fbc756cc51c68e1451995749764a62017c8a8be
ostrich <ostrich898@hotmail.com>
2026-08-29 21:05:32 +0800

    fix gitignore blocking prices.db

--- src/test_api.py
+++ src/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://data.moa.gov.tw/Service/OpenData/FromM/FarmTransData.aspx?$top=10&$skip=0&Market=%E5%8F%B0%E5%8C%97%E4%B8%80&Crop=%E7%94%98%E8%97%8D&StartDate=115.08.20&EndDate=115.08.25%"
+url = "https://data.moa.gov.tw/Service/OpenData/FromM/FarmTransData.aspx?$top=10&$skip=0&Market=%E5%8F%B0%E5%8C%97%E4%B8%80&Crop=%E7%94%98%E8%97%8D&StartDate=115.08.20&EndDate=115.08.25"
 
 
 response = requests.get(url)
